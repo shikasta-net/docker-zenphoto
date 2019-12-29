02208151c9d633b88b8e3bffaa2154b5d4bda9d4
Kym Eden <shikasta.net@gmail.com>
2019-12-29 08:48:59 +0000

    Remove local database

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ if [ ! -d $BASEDIR/zp-core ]
 then
 	rm $BASEDIR/index.html
 	# extract zenphoto files
-	/bin/tar xfz /zenphoto.tgz -C $BASEDIR --strip-components=1
+	/bin/tar xfz /zenphoto.tar.gz -C $BASEDIR --strip-components=1
 	# create config
 	/bin/cp $BASEDIR/zp-core/zenphoto_cfg.txt $BASEDIR/zp-data/zenphoto.cfg.php
 	sed -i "/mysql_user/c\$conf['mysql_user'] = 'root';" $BASEDIR/zp-data/zenphoto.cfg.php
@@ -21,24 +21,6 @@ then
 	chown www-data $BASEDIR/cache_html
 fi
 
-# initialize mysql files 
-if [ ! -d /var/lib/mysql/mysql ]
-then
-	mysqld --initialize-insecure
-fi
-
-# run mysql daemon
-/usr/bin/mysqld_safe > /dev/null 2>&1 &
-# wait for daemon to be up
-RET=1
-while [[ RET -ne 0 ]]; do
-    echo "=> Waiting for confirmation of MySQL service startup"
-    sleep 5
-    mysql -uroot -e "status" > /dev/null 2>&1
-    RET=$?
-done
-# create db
-mysql -uroot -e "CREATE DATABASE zenphoto;"
 
 # run apache in foreground
 /usr/sbin/apachectl -D FOREGROUND
